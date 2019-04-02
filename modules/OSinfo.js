513d40efbac87f2757220cb6744a6cee1c649da2
kemaron <45791175+kemaron@users.noreply.github.com>
2019-04-02 20:41:20 +0200

    Add Time converter

---
+++ modules/OSinfo.js
@@ -0,0 +1,26 @@
+// importuję moduł 'os'
+var os = require('os');
+var TimeConverter = require('../modules/TimeConverter');
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+    console.log('Formated Uptime: ', TimeConverter.print(uptime));
+}
+
+// eksportuje funkcję dla wywołujących ją
+exports.print = getOSinfo;
